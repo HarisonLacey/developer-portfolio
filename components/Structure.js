649b93e4a2a0d24755ccd834d3f1bb36d58f1b69
Harison Lacey <harisonlacey@gmail.com>
2020-08-14 11:36:24 +0200

    analytics

--- components/Structure.js
+++ components/Structure.js
@@ -13,19 +13,18 @@ const Layout = (props) => (
         crossorigin="anonymous"
       />
       <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-175432296-1"
+      />
+
+      <script
         dangerouslySetInnerHTML={{
           __html: `
-            <!-- Global site tag (gtag.js) - Google Analytics -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-175432296-1"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'UA-175432296-1');
-            </script>
-            
-              `,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-175432296-1');
+        `,
         }}
       />
     </Head>
